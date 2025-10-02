2c75eb578f0f7aa56912cbbd0ba47ae901cb6dbc
GANSYOG <gani890@gmail.com>
2025-10-02 21:45:41 +0530

    Update env.js

--- env.js
+++ env.js
@@ -2,5 +2,5 @@
 // This is for demonstration purposes only. In a real-world application,
 // you would use a backend server to proxy API requests and keep your key secret.
 window.env = {
-  GOOGLE_GEMINI_API_KEY: "YOUR_API_KEY_HERE"
+  GOOGLE_GEMINI_API_KEY: "AIzaSyCxv17Uuk1LNKSIV2seIBnI-oEHY1Rgctc"
 };
